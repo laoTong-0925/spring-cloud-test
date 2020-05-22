098adf53ea1d117d7f7a22bb2e656396ffc07229
t_t <tongxin0925@qq.com>
2020-05-22 19:29:01 +0800

    数美风控code review 日志增加

---
+++ microservice-cloud-04-consumer-product-80/src/main/java/com/mengxuegu/springcloud/ProductConsumer_80.java
@@ -0,0 +1,16 @@
+package com.mengxuegu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: t_t
+ */
+@SpringBootApplication
+public class ProductConsumer_80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductConsumer_80.class, args);
+    }
+
+}
